04b1bafbbb7346b419bf716d68872225d25594b1
g-sugai2404 <g-sugai@meitec-grp.com>
2024-05-13 15:32:08 +0900

    c language practice

---
+++ 7days-stdy/1day/list1-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(){
+    printf("123\n456\n789\n");
+    printf("シングルクオーテーション:\'ダブルクオーテーション:\"\n");
+    printf("\t円マーク\\\n");
+}
---
+++ 7days-stdy/1day/list1-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(){
+    printf("こんにちは。私の名前は%sです。\n年齢は%d歳です。\n", "菅井弦樹", 22);
+    printf("イニシャルは、%cです。\n", 'S');
+    printf("%f + %f = %f\n", 1.2, 2.7, 1.2+2.7);
+}
